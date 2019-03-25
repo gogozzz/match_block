dc61d83d2318e390f75fb724e66ad68c90769e90
sqz <632574418@qq.com>
2019-03-26 00:23:41 +0800

    ...

--- assets/Script/Plane.ts
+++ assets/Script/Plane.ts
@@ -8,7 +8,8 @@ export default class NewClass extends cc.Component {
     curClickPos: cc.Vec2;
 
     blocks: cc.Node[] = [];
-    clickedBlock: cc.Node = null;
+    clickedBlockIndex : number = -1;
+    isInTouch = false;
 
 
     // LIFE-CYCLE CALLBACKS:
@@ -38,25 +39,63 @@ export default class NewClass extends cc.Component {
     }
 
     touchMove(ev) {
-
+        if (this.isInTouch) {
+            let evPos = ev.getLocation();
+            let mvPos = this.node.convertToNodeSpaceAR(evPos);
+            let delta = mvPos.sub(this.preClickPos);
+            this.moveBlock(delta);
+            this.preClickPos = mvPos;
+        }
     }
 
     touchEnd(ev) {
         let evPos = ev.getLocation();
         this.curClickPos = this.node.convertToNodeSpaceAR(evPos);
+        this.isInTouch = false; 
     }
 
     touchStart(ev) {
         let evPos = ev.getLocation();
         this.preClickPos = this.node.convertToNodeSpaceAR(evPos);
 
-        console.log( this.preClickPos);
+        this.checkClickBlock(this.preClickPos);
+        this.isInTouch = true;
     }
 
 
     checkClickBlock(pos: cc.Vec2) {
-        
+        for (let index = 0; index < this.blocks.length; index++) {
+            const element =  this.blocks[index];
+            if (element.getBoundingBox().contains(pos)) {
+                this.clickedBlockIndex = index;
+                return element;
+            }
+        }
+    }
 
+    moveBlock(delta: cc.Vec2) {
+        let absX = Math.abs(delta.x);
+        let absY = Math.abs(delta.y);
+
+        let i = Math.ceil(this.clickedBlockIndex / 3);
+        let j = Math.ceil(this.clickedBlockIndex % 3); 
+
+        if (absX > absY) {
+            for (let index = 0; index < 3; index++) {
+                const element = this.blocks[i * 3 + index];
+                element.x += delta.x;
+                
+            }
+        }
+        else {
+            for (let index = 0; index < 3; index++) {
+                let idx = index * 3 + j;
+                const element = this.blocks[idx];
+                element.y += delta.y;
+
+                console.log(idx);
+            }
+        }
     }
 
     // update (dt) {}
